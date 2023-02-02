a57cf0468adb0fde2dfd47538d1d19cfe63b4e60
francismartinez <yofranciscomartinez@gmail.com>
2023-02-02 14:12:59 +1030

    fish - android dev

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,10 @@
 set -x PATH $HOME/.rbenv/bin $PATH
 rbenv init - | source
+
+# android dev
+set --export ANDROID $HOME/Library/Android;
+set --export ANDROID_HOME $ANDROID/sdk;
+set -gx PATH $ANDROID_HOME/tools $PATH;
+set -gx PATH $ANDROID_HOME/tools/bin $PATH;
+set -gx PATH $ANDROID_HOME/platform-tools $PATH;
+set -gx PATH $ANDROID_HOME/emulator $PATH
