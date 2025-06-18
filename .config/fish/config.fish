40738d6f675714bf9bd873481eb869f014cc288f
yofrancisco <yofranciscomartinez@gmail.com>
2025-06-18 14:10:09 +0930

    fish - orbstack

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,3 +8,7 @@ set -gx PATH $ANDROID_HOME/tools $PATH;
 set -gx PATH $ANDROID_HOME/tools/bin $PATH;
 set -gx PATH $ANDROID_HOME/platform-tools $PATH;
 set -gx PATH $ANDROID_HOME/emulator $PATH
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
